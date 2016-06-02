d2663a80819ef566f2a6fe87844e33877fde9f8c
Arian <arian2k@gmail.com>
2016-06-02 07:45:23 +0300

    Added question counter in user profile (45-1)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
       asset_path 'profile-default.png'
     end
   end
+
+  def pluralize_russian(number, krokodil, krokodila, krokodilov)
+
+    ostatok = number % 10
+    ostatok100 = number % 100
+
+    if ostatok100 >= 11 && ostatok100 <= 14
+      return "#{number} #{krokodilov}"
+    elsif ostatok == 1
+      return "#{number} #{krokodil}"
+    elsif ostatok >= 2 && ostatok <= 4
+      return "#{number} #{krokodila}"
+    elsif ostatok > 4 || ostatok == 0
+      return "#{number} #{krokodilov}"
+    end
+  end
 end
