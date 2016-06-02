0c500bcdc7661e8327446ed59b246d3ec78d8fac
Arian <arian2k@gmail.com>
2016-06-02 05:33:20 +0300

    Add #show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
 
   def show
-    @hello = 'Hello New User!'
+    @user = User.new(
+                    name: 'Vadim',
+                    username: 'installero',
+                    avatar_url: '//secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
+    )
   end
 end
