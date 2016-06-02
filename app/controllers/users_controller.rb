d188ea01cc2ab5f9d3473475a0eca0604c106411
Arian <arian2k@gmail.com>
2016-06-02 06:03:36 +0300

    Added questions partials view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,5 +14,10 @@ class UsersController < ApplicationController
         username: 'installero',
         avatar_url: '//secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
     )
+
+    @questions = [
+        Question.new(text: 'Как успехи?', created_at: Date.parse('02.06.2016')),
+        Question.new(text: 'Все гуд?', created_at: Date.parse('02.06.2016'))
+    ]
   end
 end
