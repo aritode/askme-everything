1c3e4b3e003713998940f7a6d7cd1696b2fec085
Arian <arian2k@gmail.com>
2016-06-02 06:58:26 +0300

    Added CSS and made views refactoring (L-45)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def index
+    @users = [
+        User.new(
+            id: 1,
+            name: 'Vadim',
+            username: 'installero',
+            avatar_url: '//secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
+        ),
+        User.new(
+            id: 2,
+            name: 'Misha',
+            username: 'aristofun',
+        )
+    ]
   end
 
   def new
@@ -19,5 +32,7 @@ class UsersController < ApplicationController
         Question.new(text: 'Как успехи?', created_at: Date.parse('02.06.2016')),
         Question.new(text: 'Все гуд?', created_at: Date.parse('02.06.2016'))
     ]
+
+    @new_question = Question.new
   end
 end
