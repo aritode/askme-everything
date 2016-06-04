5ccf2aa52f8f95d9e60bf9169f511876cd14a02b
Arian <arian2k@gmail.com>
2016-06-04 05:38:19 +0300

    Added auto-login to successfully registered user (46-1)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+      session[:user_id] = @user.id
+      redirect_to root_url, notice: "Пользователь @#{@user.username} успешно зарегистрирован!"
     else
       render 'new'
     end
