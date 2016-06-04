fb48582b684f7ff44560c5ee29b8d08f4e029060
Arian <arian2k@gmail.com>
2016-06-04 04:25:25 +0300

    Added Users and Sessions controller with custom views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+
   root 'users#index'
 
   resources :users, except: [:destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :questions
 
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
