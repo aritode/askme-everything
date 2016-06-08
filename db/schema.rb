9cdf5c538ad1a50c94e2e437bd4ae703a6dc63d8
Arian <arian2k@gmail.com>
2016-06-08 10:35:56 +0300

    Added Choosing header color for User profile (46-2)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601020939) do
+ActiveRecord::Schema.define(version: 20160608061718) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "text"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160601020939) do
     t.string   "password_hash"
     t.string   "password_salt"
     t.string   "avatar_url"
+    t.string   "header_color"
   end
 
 end
